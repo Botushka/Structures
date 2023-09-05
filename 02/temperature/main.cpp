ca270b7f6932f71f183b7a7b125550086882c79b
BuildTools <91798274+Botushka@users.noreply.github.com>
2023-09-05 23:15:12 +0300

    02

---
+++ 02/temperature/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter a temperature: ";
+    double temperature;
+    cin >> temperature;
+    cout << temperature << " degrees Celsius is " << (temperature * 1.8) + 32 << " degrees Fahrenheit" << endl;
+    cout << temperature << " degrees Fahrenheit is " << (temperature - 32) / 1.8 << " degrees Celsius" << endl;
+
+    return 0;
+}
